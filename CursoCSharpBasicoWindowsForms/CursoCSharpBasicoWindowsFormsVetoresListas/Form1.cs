c8f3974a9548f8f2f7d296fb2903169d4b3fc08b
fabiodevigilli <55096053+fabiodevigilli@users.noreply.github.com>
2020-05-03 15:22:57 -0300

    Add files via upload

---
+++ CursoCSharpBasicoWindowsForms/CursoCSharpBasicoWindowsFormsVetoresListas/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoCSharpBasicoWindowsFormsVetoresListas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome = "Teste1";
+
+            // Vetor é estático
+            string[] nomes = {"Teste2", "Teste3", "Teste4"};
+
+            string[] nomes2 = new string[3];
+            nomes2[0] = "Teste5";
+            nomes2[1] = "Teste6";
+            nomes2[2] = "Teste7";
+
+            // Lista é dinâmica
+            List<string> nomes3 = new List<string>();
+            nomes3.Add("Teste8");
+            nomes3.Add("Teste9");
+            nomes3.Add("Teste10");
+            //nomes3.Add("Teste11");
+            //nomes3.Add("Teste12");
+            //nomes3.Remove("Teste11");
+            //nomes3.Remove("Teste12");
+
+            comboBox1.DataSource = nomes3;
+        }
+    }
+}
