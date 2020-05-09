83f5e59407e6b4fd178e4bff393ac8e745670183
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2020-05-09 20:11:31 -0300

    FINALIZADO A AULA 5

---
+++ CursoCSharpContinueGold/CursoCSharpContinueGoldAula5/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharpContinueGoldAula5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AULA 05.01 - DELEGATES E EVENTOS
+            // Aqui instanciamos o objeto delegate
+            // dentro do construtor do delegate vai pedir um ponteiro para um método que neste caso é static
+            // Saudar s = new Saudar(DizerTchau);
+            // s("Pedro");
+            // Podemos criar também os multicasts delegates que possuem diversas referencias a metodos diferentes
+            //Saudar a, b, c, d;
+            //a = DizerOi;
+            //b = DizerTchau;
+            //c = a + b;
+            //d = c - a;
+            //a("A");
+            //b("B");
+            //c("C");
+            //d("D");
+
+
+            //AULA 05.02 - LÂMBDA EXPRESSIONS, MÉTODOS ANÔNINOS E DELEGATES
+            // 1º exemplo com delegate
+            CriarMensagem c = Saudar;
+            c("C");
+            //2º exemplo com métodos anônimos
+            CriarMensagem d = delegate (string nome)
+            {
+                Console.WriteLine("Olá" + nome);
+            };
+            // no 2º exemplo, só é executado, qdo invocado, conforme abaixo:
+            d("D");
+            //3º exemplo com expressão lâmbda
+            CriarMensagem e = (nome)=>
+            {
+                Console.WriteLine("Olá" + nome);
+            };
+            e("E");
+
+            List<Produto> produtos = Produto.GetProdutos();
+            // produtosCaros com delegate
+            List<Produto> produtosCaros = produtos.Where(FiltrarProdutosCaros).ToList();
+            // produtosCaros com lambda
+            List<Produto> produtosCarosLambda = produtos.Where(x => x.Preco > 25).ToList();
+
+        }
+        //AULA 05.01 - DELEGATES E EVENTOS
+        static void DizerOi(string nome)
+        {
+            Console.WriteLine("Oi" + nome);
+        }
+
+        static void DizerTchau(string nome)
+        {
+            Console.WriteLine("Tchau" + nome);
+        }
+
+        //AULA 05.02 - LÂMBDA EXPRESSIONS, MÉTODOS ANÔNINOS E DELEGATES
+        static void Saudar(string nome)
+        {
+            Console.WriteLine("Olá" + nome);
+        }
+
+        static bool FiltrarProdutosCaros(Produto p)
+        {
+            return p.Preco > 25;
+        }
+
+    }
+    //AULA 05.01 - DELEGATES E EVENTOS
+    // delegate aceita um ponteiro para uma função
+    public delegate void Saudar(string nome);
+
+    //AULA 05.02 - LÂMBDA EXPRESSIONS, MÉTODOS ANÔNINOS E DELEGATES
+    public delegate void CriarMensagem(string nome);
+
+
+    public class Produto
+    {
+        public static List<Produto> GetProdutos()
+        {
+            return new List<Produto>()
+            { // AQUI CRIAMOS UMA LISTAGEM
+                new Produto()
+                {
+                    Id = 1, Descricao = "A",
+                    Preco = 20, QtdeEstoque = 40
+                },
+                new Produto()
+                {
+                    Id = 2, Descricao = "B",
+                    Preco = 30, QtdeEstoque = 40
+                },
+                new Produto()
+                {
+                    Id = 3, Descricao = "C",
+                    Preco = 40, QtdeEstoque = 40
+                }
+            };
+        }
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public double QtdeEstoque { get; set; }
+    }
+}
---
+++ CursoCSharpContinueGold/CursoCSharpContinueGoldAula5WinForms/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoCSharpContinueGoldAula5WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // AULA 5.03 - EVENTOS
+            // exemplo 01
+            TextBox txt = new TextBox();
+            txt.Location = new Point(100, 100);
+            txt.Width = 200;
+            this.Controls.Add(txt);
+            // usando expressões lambdas
+            txt.Leave += (obj, e) =>
+            {
+                txt.TextAlign = HorizontalAlignment.Center;
+            };
+
+            //exemplo 02 - usando delegate
+            ForeCast f = new ForeCast();
+            f.InvernoChegou += F_InvernoChegou;
+            f.VeraoChegou += F_VeraoChegou;
+            f.TrocarTemperatura(20);
+            f.TrocarTemperatura(5);
+            f.TrocarTemperatura(35);        
+        }
+
+        // AULA 5.03 - EVENTOS
+        private void F_VeraoChegou(int temperatura)
+        {
+            MessageBox.Show("Verão chegou e está" + temperatura + "º");
+        }
+
+        private void F_InvernoChegou(int temperatura)
+        {
+            MessageBox.Show("Inverno chegou e está" + temperatura + "º");
+        }
+
+        // AULA 5.04 - DELEGATE ASSÍNCRONO
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileManager manager = new FileManager(new FileStream("teste10.txt", FileMode.Create));
+            WriteFile write = new WriteFile(manager.WriteContent);
+            write.BeginInvoke("Olá, tudo bem?", manager.EndWrite, null);
+            MessageBox.Show("Test");
+        }
+    }
+    // AULA 5.03 - EVENTOS
+    public class ForeCast
+    {
+        public int Temperatura { get; private set; }
+
+        public event TrocarEstacao VeraoChegou;
+        public event TrocarEstacao InvernoChegou;
+
+        public void TrocarTemperatura(int temperatura)
+        {
+            this.Temperatura = temperatura;
+            if (temperatura > 32 && VeraoChegou != null)
+            {
+                VeraoChegou(temperatura);
+            }
+            else if (temperatura < 14 && InvernoChegou != null)
+            {
+                InvernoChegou(temperatura);
+            }
+        }
+    }
+    public delegate void TrocarEstacao(int temperatura);
+
+
+    // AULA 5.04 - DELEGATE ASSÍNCRONO
+    public delegate void WriteFile(string file);
+
+    public class FileManager
+    {
+        private FileStream fs = null;
+        public FileManager(FileStream file)
+        {
+            this.fs = file;
+        }
+
+        public void WriteContent(string content)
+        {
+            if (fs != null)
+            {
+                byte[] dados = Encoding.UTF8.GetBytes(content);
+                fs.Write(dados, 0, dados.Length);
+            }
+        }
+
+        public void EndWrite(IAsyncResult result)
+        {
+            try
+            {
+                if (fs.Length == 0)
+                {
+                    throw new Exception("Erro na escrita");
+                }
+            }
+            finally
+            {
+                fs.Dispose();
+            }            
+        }
+    }
+}
