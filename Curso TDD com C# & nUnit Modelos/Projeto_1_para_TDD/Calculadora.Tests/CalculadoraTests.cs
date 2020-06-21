8dbbc74f58fce2cba07c3cc5ba3cbad84d30c30f
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2020-06-21 17:13:22 -0300

    commit inicial

---
+++ Curso TDD com C# & nUnit Modelos/Projeto_1_para_TDD/Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraTests
+    {
+        [TestFixture]
+        public class CalculadoraSimplesTests
+        {
+            /*Teste Classe Adicionar dois números*/
+            [Test]
+            public void DeveAdicionarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Adicionar(5,5);
+
+                Assert.That(resultado, Is.EqualTo(10));
+            }
+
+            /*Teste Classe Subtrair dois números*/
+            [Test]
+            public void DeveSubtrairDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Subtrair(10, 5);
+
+                Assert.That(resultado, Is.EqualTo(5));
+            }
+
+            /*Teste Classe Multiplicar dois números - neste caso, irá falhar*/
+            [Test]
+            public void DeveMultiplicarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Multiplicar(5, 3);
+
+                Assert.That(resultado, Is.EqualTo(15));
+            }
+
+            /*Teste Classe Dividir dois números*/
+            [Test]
+            public void DeveDividirDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Dividir(10, 5);
+
+                Assert.That(resultado, Is.EqualTo(2));
+            }
+
+        }
+    }
+}
---
+++ Curso TDD com C# & nUnit Modelos/Projeto_1_para_TDD/Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        /* Método para adicionar dois números*/
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        /* Método para subtrair dois números*/
+        public int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        /* Método para multiplicar dois números*/
+        public int Multiplicar(int num1, int num2)
+        {
+            // Para fins de demo (bug) => deveria ser return num1 * num2;
+            return num1 + num2;
+        }
+
+        /* Método para subtrair dois números*/
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
