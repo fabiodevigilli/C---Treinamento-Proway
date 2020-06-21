816bdd6dc54500d11a2fff3a03b0c366fc04069a
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2020-06-21 18:48:13 -0300

    Filanizado Aula 08 - String Asserts

---
+++ Curso TDD com C# & nUnit Modelos/Demo_Asserts/Demo_Asserts.Tests/Igualdade/JuntarNomesTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Demo_Asserts.Tests.Igualdade
+{
+    [TestFixture]
+    public class JuntarNomesTests
+    {
+        /* Método de teste finalidade: Comparar duas strings iguais */
+        [Test]
+        public void DevoJuntarNomeCompleto()
+        {
+            var sut = new JuntarNomes();
+
+            var nomeCompleto = sut.Juntar("João", "Silva");
+
+            Assert.That(nomeCompleto, Is.EqualTo("João Silva"));
+        }
+
+        /* Método de teste finalidade: Comparar duas strings em case sensitive */
+        [Test]
+        public void DevoJuntarNome_Completo_CaseSensitive()
+        {
+            var sut = new JuntarNomes();
+
+            var nomeCompleto = sut.Juntar("joão", "silva");
+
+            Assert.That(nomeCompleto, Is.EqualTo("JOÃO SILVA").IgnoreCase);
+        }
+
+        /* Método de teste finalidade: Comparar duas strings diferentes */
+        [Test]
+        public void DevoJuntarNomeCompleto_NaoIgual()
+        {
+            var sut = new JuntarNomes();
+
+            var nomeCompleto = sut.Juntar("João", "Silva");
+
+            Assert.That(nomeCompleto, Is.Not.EqualTo("José Silva"));
+        }
+    }
+}
---
+++ Curso TDD com C# & nUnit Modelos/Demo_Asserts/Demo_Asserts/JuntarNomes.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo_Asserts
+{
+    public class JuntarNomes
+    {
+        /* Método principal correspondente a Juntar o Nome + Sobrenome */
+        public string Juntar(string nome, string sobrenome)
+        {
+            return nome + " " + sobrenome;
+        }
+    }
+}
