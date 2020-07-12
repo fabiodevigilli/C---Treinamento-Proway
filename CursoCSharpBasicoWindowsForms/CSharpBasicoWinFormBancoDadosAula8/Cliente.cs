6e18c620e19da55ecc6cc421739305c784526753
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2020-07-12 10:21:04 -0300

    Ajustes em usings desnecessários

--- CursoCSharpBasicoWindowsForms/CSharpBasicoWinFormBancoDadosAula8/Cliente.cs
+++ CursoCSharpBasicoWindowsForms/CSharpBasicoWinFormBancoDadosAula8/Cliente.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpBasicoWinFormBancoDadosAula8
+﻿namespace CSharpBasicoWinFormBancoDadosAula8
 {
     class Cliente
     {
